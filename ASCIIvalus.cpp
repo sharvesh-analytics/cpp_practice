a1a74e4ed11819eb408c147e6b381094ad9b748b
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-06-12 22:27:09 +0530

    Add files via upload

---
+++ ASCIIvalus.cpp
@@ -0,0 +1,22 @@
+// To show char to int
+// a=97,b=98,c=99
+// A=65,B=66,C=67
+// 0=48,1=49,2=50
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     char x;
+//     cin>>x;
+//     cout<<(int)x;
+
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    int x=(int)ch;
+    cout<<x;
+}
---
+++ Basics.cpp
@@ -0,0 +1,12 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"my name is sharvesh pandey";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"hello cpp"<<"\n";
+//     cout<<"hello cpp";
+// }
