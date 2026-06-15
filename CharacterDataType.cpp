6da944625ca7d8ee686ce80cd6ee6b5b88c231b2
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-06-15 23:31:40 +0530

    Add files via upload

---
+++ CharacterDataType.cpp
@@ -0,0 +1,10 @@
+// int= 1 2 3 4 5
+// float=3.4,5.5,7.6
+// ch= a,x,b,y 
+#include <iostream>
+using namespace std;
+int main(){
+    char x;
+    cin>>x;
+    cout<<x;
+}
---
+++ CompositeOrNot.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    for(int i=2;i<=n/2;i++){
+        if(n%i==0){
+            cout<<"Composite";}
+            else
+            cout<<"not composite";
+            break;
+        
+    }
+}
