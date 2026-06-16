3ccef66d7c6a539b61afae6c0ea1fb6c417bbddb
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-06-16 15:49:09 +0530

    Add files via upload

---
+++ Continue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+   
+    
+    for(int i=1;i<=100;i++){
+        if(i==3) continue;
+        if(i==19) continue;
+        cout<<i<<" ";
+
+    }
+    
+    
+}
