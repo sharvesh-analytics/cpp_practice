8227cf9afce738eb1dc345fc5ddfac44dfbafc8d
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-06-17 22:50:05 +0530

    Add files via upload

---
+++ example1.cpp
@@ -0,0 +1,9 @@
+// we find out area of circle
+#include <iostream>
+using namespace std;
+int main(){
+    float r = 4;
+    float a = 3.1415*r*r;
+    
+    cout<<"AREA OF CIRCLE: "<<a<<endl ;
+}
---
+++ example2.cpp
@@ -0,0 +1,12 @@
+// calculation simple intrest
+#include <iostream>
+using namespace std;
+int main(){
+    float p,r,t;
+    float si=p*r*t/100;
+    p=1111;
+    r=30;
+    t=3;
+    cout<<"THE SIMPLE INTREST IS: "<<si<<endl;
+
+ }
