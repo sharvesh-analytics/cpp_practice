188c331d0b010f4dfe44027a929c3f3cb9c71b19
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-06-20 22:33:26 +0530

    Add files via upload

---
+++ Loops.cpp
@@ -0,0 +1,12 @@
+// LOOPS
+// 1.FOR LOOP, 2.BREAK AND CONTINUE, 3.WHILE AND DO WHILE LOOP,
+// 4.QUESTIONS USING OPERATOR, 5.PATTERN PRINTING PROBLEMS
+// CONTINUE PRINTING ANYTHING LIKE "HELLO WORLD" AND ANY NUMBER 1 TO 10
+
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=8;i++){
+        cout<<"hello sharvesh"<<endl;
+    }
+}
