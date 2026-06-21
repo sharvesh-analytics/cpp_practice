e510304eaea588c32a01b56e1796085a63000770
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-06-21 22:52:32 +0530

    Add files via upload

---
+++ ModulesOperatorsAndTypecasting.cpp
@@ -0,0 +1,15 @@
+// . When a%b is remainder when a isdivided by b
+// . Some Important points
+// . a%b=a[if a<b]
+// . a%(-b)=a%blanks
+// . (-a)%b=[a%b]
+// By using typcasting
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<x/2;
+    cin>>x;
+    float y= (float)x;
+    cout<<y/2;
+}
