619d85578cedcc69941230f27d97cf12b8aa3b9f
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-06-22 23:05:45 +0530

    Add files via upload

---
+++ Operators.cpp
@@ -0,0 +1,36 @@
+//INCREAMENT AND DECREAMENT OPERATOR
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x=5;
+//     x++;
+//     cout<<x<<endl;
+//     x--;
+//     cout<<x<<endl;
+//     ++x;
+//     cout<<x<<endl;
+//     --x;
+//     cout<<x<<endl;
+// }
+
+//post incremental
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x=4;
+//     cout<<x<<endl;
+//     cout<<x++<<endl;
+//     cout<<x<<endl;
+// }
+
+//pre incremental
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int x=5;
+    cout<<x<<endl;
+    cout<<++x<<endl;
+    cout<<x<<endl;
+}
