de6417384abf5478927dc0be83e40986d7a7ae31
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-06-29 22:39:09 +0530

    Add files via upload

---
+++ SumInFunctions.cpp
@@ -0,0 +1,21 @@
+// hm yh pr sum nikalna sikh rhe hai by functions 
+
+// #include <iostream>
+// using namespace std;
+// int sum(int a,int b){
+//     return a+b;
+
+// }
+// int main(){
+//     cout<<sum(7,99);
+// }
+
+#include <iostream>
+using namespace std;
+void sum(int a , int b){
+    cout<<a+b;
+}
+int main(){
+    sum(44,45);
+
+}
