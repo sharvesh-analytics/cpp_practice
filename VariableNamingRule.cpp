862aa633ae6963daf4a61235fcffe0084588c421
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-07-08 23:01:34 +0530

    Add files via upload

---
+++ VariableNamingRule.cpp
@@ -0,0 +1,16 @@
+//Varibale Naming Rules
+//variable cant start from an alphabet and underscore
+//special character except _ and $ are not allowed
+//some particular keyword are not allowed
+//commas or blanks are not allowed
+
+// a g h H Y I alphabts
+//1 5 3 4  6
+// <> , . / ?: " ; '" { }[ ] \  |  + _ ) ( ) * & ^ % $ # @ !
+
+#include <iostream>
+using namespace std;
+int main(){
+    int y=3;
+    cout<<y;
+}
