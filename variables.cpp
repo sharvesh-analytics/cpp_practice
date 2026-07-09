e01e1f39269736f7a4884123f5232ee866449279
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-07-09 23:07:21 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,49 @@
+// variables and their declaration
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"4"<<endl;
+//     cout<<4<<endl;
+//     cout<<4*3<<endl;
+//     cout<<"4+3"<<endl;
+// }
+
+// input variable
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x,y;
+//     x=4;
+//     y=5;
+  
+//     cout<<x*y<<endl;
+//     cout<<x+y<<endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x=3;
+//     cout<<x<<endl;
+//     x=x+2;
+//     x+=2;
+//     cout<<x<<endl;
+//     x=x-4;
+//     x-=3;
+//     cout<<x<<endl;
+//     x*=3;
+//     cout<<x<<endl;
+// }
+
+// Floating no. data type
+
+#include <iostream>
+using namespace std;
+int main(){
+    float x=5;
+    float y=2;
+    cout<<x+y<<endl;
+    cout<<x/y<<endl;
+    
+}
