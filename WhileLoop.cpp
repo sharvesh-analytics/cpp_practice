e23addcf3b5d82dec799c1127dfbbb4b44ed32fa
SHARVESH PANDEY <pandeysharvesh0410@gmail.com>
2026-07-15 11:25:03 +0530

    Add files via upload

---
+++ WhileLoop.cpp
@@ -0,0 +1,11 @@
+//   any condition that was return in step by step process is clled While loop 
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i=1;
+    while(i<=100){
+        cout<<i<<" ";
+        i++;
+    }
+}
